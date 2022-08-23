40e3b1f66cb490fcf4ae80bfca479cbfaac5fa22
Juhee Seong <arkshart2@gmail.com>
2022-08-23 20:36:43 +0900

    DropRP Script Comment

--- Assets/Scripts/DropRP.cs
+++ Assets/Scripts/DropRP.cs
@@ -1,21 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class DropRP : MonoBehaviour
 {
-    int click;
-    int rpLayerMask;
-    public Vector3 vel;
+    int rpLayerMask;    //레이어 마스크
+    public Vector3 vel; //하강 속도
 
     public void Awake()
     {
         rpLayerMask = 1 << LayerMask.NameToLayer("RP");
     }
+
+    void Update()
+    {
+        if (Time.timeScale == 0) return;    //일시정지 중이라면 작동하지 않는다.
+
+        if (Input.GetMouseButtonDown(0))    //터치 지점에 자신이 있다면 RP를 준다.
+        {
+            Vector2 touchPos;
+            if (Input.touchCount > 0) touchPos = Input.touches[0].position;
+            else touchPos = Input.mousePosition;
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touchPos), Vector2.zero, 0f, rpLayerMask);
+            if (hit.collider != null && hit.collider.gameObject == gameObject) GiveRP();
+        }
+
+        //하강한다. 너무 아래로 내려오면 사라진다.
+        transform.Translate(vel * Time.unscaledDeltaTime);
+        if (transform.position.y < Camera.main.transform.position.y - 16.0f)
+        {
+            BattleManager.instance.dropRPs.Remove(this);
+            GameManager.instance.ReturnDropRPToPool(this);
+        }
+    }
+
+
+    //초기 위치와 유물에 의한 하강속도를 정한다.
     public void InitializeDropRP()
     {
-        click = 0;
         transform.position = Camera.main.transform.position + new Vector3(Random.Range(-5, 5), 16, 2);
         if (GameManager.instance.baseRelics[18].have)
         {
@@ -25,34 +46,11 @@ public class DropRP : MonoBehaviour
         else vel = new Vector3(0, -2.0f, 0);
     }
 
-    void Update()
+    //RP를 주고 게임에서 제거한다.
+    void GiveRP()
     {
-        if (Time.timeScale == 0) return;
-        if (Input.GetMouseButtonUp(0))
-        {
-            Vector2 touchPos;
-            if (Input.touchCount > 0) touchPos = Input.touches[0].position;
-            else touchPos = Input.mousePosition;
-
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touchPos), Vector2.zero, 0f, rpLayerMask);
-            if (hit.collider != null && hit.collider.gameObject == gameObject) UpdateClick();
-        }
-
-        transform.Translate(vel * Time.unscaledDeltaTime);
-        if (transform.position.y < Camera.main.transform.position.y - 16.0f)
-        {
-            BattleManager.instance.dropRPs.Remove(this);
-            GameManager.instance.ReturnDropRPToPool(this);
-        }
-    }
-
-    public void UpdateClick()
-    {
-        if (++click > 0)
-        {
-            BattleManager.instance.ChangePlayerRP(Random.Range(3, 6));
-            BattleManager.instance.dropRPs.Remove(this);
-            GameManager.instance.ReturnDropRPToPool(this);
-        }
+        BattleManager.instance.ChangePlayerRP(Random.Range(3, 6));
+        BattleManager.instance.dropRPs.Remove(this);
+        GameManager.instance.ReturnDropRPToPool(this);
     }
 }
